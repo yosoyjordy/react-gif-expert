7b22ce2b929d1bde6a5999114c3bbf22928be2ec
Jordy Palencia <macbookpro@Jordys-Macbook-Pro.local>
2022-11-28 11:13:48 -0600

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+export const AddCategory = ({onNewCategory}) => {
+    const [InputValue, setInputValue] = useState('')
+
+    const onInputChange = ({target}) => {
+      setInputValue(target.value);
+    }
+
+    const onSubmit = (event) => {
+      event.preventDefault();
+      if(!InputValue) return;
+      onNewCategory(InputValue);
+      setInputValue('');
+    }
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text"
+          placeholder="Add category"
+          value= {InputValue}
+          onChange={onInputChange}
+          />
+      </form>
+    )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+const API_KEY = '2A0CVEwlhE8V60n1QJTHOCKqk3n51YGV';
+const LIMIT = 10;
+
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=${LIMIT}`
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(gif => ({
+        id: gif.id,
+        title: gif.title,
+        url: gif.images.downsized_medium.url
+    }));
+    
+    return gifs;
+}
